1448e98149a84227ad51bbdd9244419b46956612
Edwin Fom <front@datnek.be>
2025-07-21 15:14:54 +0100

    03:Trpc Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+import { useTRPC } from '@/trpc/client'
 import React from 'react'
 
 const Page = () => {
+
+  const trpc = useTRPC();
+
+  trpc.hello.queryOptions({ text: "123" })
+
   return (
     <div>
       Hello World
